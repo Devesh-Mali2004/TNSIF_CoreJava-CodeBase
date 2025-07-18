e5b032186dafc8a3fc609c409b8efe91fa5fc1f6
Devesh-Mali2004 <deveshmali2004@gmail.com>
2025-07-18 23:43:41 +0530

    Commit By Devesh Mali

--- Day_01/HelloWorld.java
+++ Day_01/HelloWorld.java
@@ -3,6 +3,6 @@ package Day_01;
 public class Helloworld
 {
 public static void main(String[] args) {
-	System.out.println("Hello, I am Vinay...");
+	System.out.println("Hello, I am Devesh...");
 }
 }
